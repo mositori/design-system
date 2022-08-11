3cbaddac873051a75adbc79defcb5527e29f5798
mositori <destrage1958@gmail.com>
2022-08-11 18:30:36 +0900

    version up

--- packages/color-palette/scripts/build.ts
+++ packages/color-palette/scripts/build.ts
@@ -1,40 +1,40 @@
-import { build, BuildOptions } from "esbuild"
-import pkg from "../package.json"
+import { build, BuildOptions } from 'esbuild';
+import pkg from '../package.json';
 
 const shared: BuildOptions = {
-    logLevel: 'info',
-    charset: 'utf8',
-    minify: true,
-    bundle: true,
-    platform: 'node',
-    define: {
-        VERSION: JSON.stringify(pkg.version),
-    }
-}
+	logLevel: 'info',
+	charset: 'utf8',
+	minify: true,
+	bundle: true,
+	platform: 'node',
+	define: {
+		VERSION: JSON.stringify(pkg.version),
+	},
+};
 
 const p = [
-    build({
-        ...shared,
-        format: 'esm',
-        entryPoints: ['src/index.ts'],
-        outfile: pkg.exports['.'].import,
-    }),
-    build({
-        ...shared,
-        format: 'cjs',
-        entryPoints: ['src/index.ts'],
-        outfile: pkg.exports['.'].require,
-    }),
-    build({
-        ...shared,
-        format: 'esm',
-        platform: 'browser',
-        entryPoints: ['src/index.ts'],
-        outfile: pkg.exports['.'].browser,
-    })
-]
+	build({
+		...shared,
+		format: 'esm',
+		entryPoints: ['src/index.ts'],
+		outfile: pkg.exports['.'].import,
+	}),
+	build({
+		...shared,
+		format: 'cjs',
+		entryPoints: ['src/index.ts'],
+		outfile: pkg.exports['.'].require,
+	}),
+	build({
+		...shared,
+		format: 'esm',
+		platform: 'browser',
+		entryPoints: ['src/index.ts'],
+		outfile: pkg.exports['.'].browser,
+	}),
+];
 
 await Promise.all(p).catch(err => {
-    console.error(err.stack || err)
-    throw err
-})
+	console.error(err.stack || err);
+	throw err;
+});
