6eb099145d991687b3d68541350c97008d195329
mositori <destrage1958@gmail.com>
2022-08-11 17:30:24 +0900

    init

---
+++ packages/color-palette/src/index.ts
@@ -0,0 +1,76 @@
+export const Mono = {
+    Mono50: "#fafafa",
+    Mono100: "#f5f5f5",
+    Mono200: "#e5e5e5",
+    Mono300: "#d4d4d4",
+    Mono400: "#a3a3a3",
+    Mono500: "#737373",
+    Mono600: "#525252",
+    Mono700: "#404040",
+    Mono800: "#262626",
+    Mono900: "#171717",
+    Mono1000: "#101010",
+  } as const;
+  
+  export const Red = {
+    Red50: "#fef2f2",
+    Red100: "#fee2e2",
+    Red200: "#fecaca",
+    Red300: "#fca5a5",
+    Red400: "#f87171",
+    Red500: "#ef4444",
+    Red600: "#dc2626",
+    Red700: "#b91c1c",
+    Red800: "#991b1b",
+    Red900: "#7f1d1d"
+  } as const;
+  
+  export const Blue = {
+      Blue50: "#eff6ff",
+      Blue100: "#dbeafe",
+      Blue200: "#bfdbfe",
+      Blue300: "#93c5fd",
+      Blue400: "#60a5fa",
+      Blue500: "#3b82f6",
+      Blue600: "#2563eb",
+      Blue700: "#1d4ed8",
+      Blue800: "#1e40af",
+      Blue900: "#1e3a8a"
+  } as const
+  
+  export const Green = {
+    Green50: "#EBF7F2",
+    Green100: "#D6EFE4",
+    Green200: "#ADDFC9",
+    Green300: "#85D0AE",
+    Green400: "#5CC093",
+    Green500: "#33B178",
+    Green600: "#009D56",
+    Green700: "#0C9054",
+    Green800: "#187F50",
+  } as const;
+  
+  export const Orange = {
+    Orange50: "#FEF3EB",
+    Orange100: "#FCE6D6",
+    Orange200: "#F9CCAE",
+    Orange300: "#F6B386",
+    Orange400: "#F39A5E",
+    Orange500: "#F18135",
+    Orange600: "#ED6103",
+    Orange700: "#D24E00",
+    Orange800: "#B04100",
+  } as const;
+  
+  export const Yellow = {
+    Yellow50: "#FFF8EB",
+    Yellow100: "#FFF1D6",
+    Yellow200: "#FFE4AD",
+    Yellow300: "#FFD785",
+    Yellow400: "#FFC95C",
+    Yellow500: "#FFBC33",
+    Yellow600: "#FFAB00",
+    Yellow700: "#D79000",
+    Yellow800: "#AE7400",
+  } as const;
+  
